ee9873ed240b85fe746a7076ad9b08362d009ad9
Andrii Trybynenko <tamerjlah@gmail.com>
2017-03-12 13:47:54 +0100

    Git api added

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import GitHub from 'github-api';
+import { username, password } from 'auth';
 
-var Component = React.createClass({
-  render: function () {
+let Component = React.createClass({
+
+  getInitialState() {
+    let gh = new GitHub({
+      username,
+      password,
+      /* also acceptable:
+       token: 'MY_OAUTH_TOKEN'
+       */
+    });
+
+    let tamer1an = gh.getUser('tamer1an');
+
+    tamer1an.listStarredRepos(function(err, repos) {
+      // look at all the starred repos!
+
+      console.log('THE REPOS', repos.slice(1,6));
+    });
+
+    return {
+      user: tamer1an
+    };
+  },
+
+  render () {
     if (this.props.onRender) {
       this.props.onRender();
     }
@@ -20,3 +45,8 @@ var Component = React.createClass({
 });
 
 export default Component;
+
+// let me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
+// me.listNotifications(function(err, notifications) {
+//   // do some stuff
+// });
